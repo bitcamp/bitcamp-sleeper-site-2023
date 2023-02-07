4f24f94183296f0af39d42be037bbc4f26cf6dae
Jason Liu <jasoneliu03@gmail.com>
2023-02-06 22:38:02 -0500

    Add sponsorship prospectus to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -8,9 +8,15 @@
 
     <div class="floatie-text">
       <div class="links">
-        <a href="http://mlh.io/code-of-conduct">MLH Code of Conduct</a> |
-        <a href="/brand.pdf">Brand Guidelines</a> |
-        <a href="mailto:hello@bit.camp">Contact Us</a>
+        <span class="links-1">
+          <a href="http://mlh.io/code-of-conduct">MLH Code of Conduct</a> |
+          <a href="/prospectus.pdf">Sponsor Us</a></span
+        >
+        <span class="links-separator"> | </span>
+        <span class="links-2">
+          <a href="/brand.pdf">Brand Guidelines</a> |
+          <a href="mailto:hello@bit.camp">Contact Us</a>
+        </span>
       </div>
       <div class="copyright">
         Copyright &copy; 2019 - 2023 Bitcamp. All Rights Reserved.
@@ -84,7 +90,16 @@ footer {
 
 @media (max-width: 768px) {
   .floatie-text {
-    font-size: 2.5vw;
+    font-size: 3vw;
+  }
+
+  .links-1,
+  .links-2 {
+    display: block;
+  }
+
+  .links-separator {
+    display: none;
   }
 }
 </style>
